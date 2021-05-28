a5801b79677b52575250a1b5ec8b7ba44f91ffe9
Dragos <dragos360@yahoo.com>
2021-05-28 13:33:26 +0300

    basic weksocket implementation

--- resources/js/dashboard/views/HomeView.vue
+++ resources/js/dashboard/views/HomeView.vue
@@ -1,10 +1,21 @@
 <template>
-    <p>Home</p>
+    <div>
+        {{message}}
+    </div>
 </template>
 
 
 <script>
     export default {
-        
+        mounted() {
+            window.Echo.channel('home')
+                        .listen('NewMessage', (e) => {this.message = e.message});
+        },
+
+        data() {
+            return {
+                message: ''
+            }
+        },
     }
 </script>
