c9bc0f23440d574498ceb64c42517593de1a41d6
Dragos <dragos360@yahoo.com>
2021-06-07 14:04:35 +0300

    improved pagination fixed categoires routes bug

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::group(['middleware'=>'auth:sanctum', 'namespace'=>'Api\Dashboard', 'prefi
     Route::apiResource('users', 'UserController');
     Route::apiResource('roles', 'RoleController');
     Route::apiResource('products', 'ProductController');
-    Route::apiResource('categories', 'CategoryController');
+    Route::apiResource('categories', 'CategoryController')->except('show');
     Route::apiResource('units', 'UnitController');
     Route::apiResource('ingredients', 'IngredientController');
     Route::apiResource('stocks', 'StockController')->only('update');
