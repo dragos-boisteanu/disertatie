8c0a327f05cee07b69fe21dd76bea9e47d3ca14b
Dragos <dragos360@yahoo.com>
2021-06-09 21:10:45 +0300

    refresh tables list

--- resources/js/dashboard/views/tables/TablesView.vue
+++ resources/js/dashboard/views/tables/TablesView.vue
@@ -7,7 +7,34 @@
       @closed="toggleModal"
     ></ConfirmTableDeleteModal>
 
-    <template slot="header"> Tables </template>
+    <template slot="header">
+      <div class="flex items-center justify-between md:justify-start gap-x-4">
+        <span> Tables </span>
+        <button
+          @click="refresh"
+          class="
+            p-1
+            bg-lightBlue-600
+            rounded-sm
+            active:shadow-inner
+            active:bg-lightBlue-500
+          "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#ffffff"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </button>
+      </div>
+    </template>
 
     <div class="w-full md:flex md:gap-x-4 xl:w-3/4 2xl:w-1/2">
       <div class="flex flex-col bg-white shadow rounded-sm p-5 md:flex-1">
@@ -83,7 +110,7 @@
             </div>
 
             <Button v-if="isTableSelected" type="primary"> Update </Button>
-            <Button v-else  type="primary" @click.native="createNewTable">
+            <Button v-else type="primary" @click.native="createNewTable">
               Submit
             </Button>
           </div>
@@ -144,11 +171,16 @@ export default {
 
   methods: {
     ...mapActions("Tables", [
+      "downloadTables",
       "storeTable",
       "disableTable",
       "restoreTable",
       "deleteTable",
     ]),
+
+    async refresh() {
+      await this.downloadTables();
+    },
 
     async createNewTable() {
       const table = await this.storeTable(this.table.name);
