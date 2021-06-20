f63b2136276fae549141b87b11936cfc0b157782
dragos720 <159753852456Gh>
2021-06-20 15:34:11 +0300

    header, nav, auth modals looks good

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,5 @@
 <header class="flex-initial h-80 bg-gray-900">
+    <div id="backdrop" class="fixed z-20 top-0 left-0 h-full w-full bg-black bg-opacity-75 hidden"></div>
     <div class="splide">
         <div class="splide__track">
             <ul class="splide__list">
@@ -8,6 +9,7 @@
             </ul>
         </div>
     </div>
+    {{-- mobile nav --}}
     <nav class="h-10 bg-orange-600 flex items-center justify-between px-4 lg:hidden">
         <div id="mobileMenuToggler" class="p-3 cursor-pointer">
             @for ($i = 0; $i < 3; $i++)
@@ -21,12 +23,7 @@
                     d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.37-.66-.11-1.48-.87-1.48H5.21l-.94-2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
             </svg>
         </div>
-        <ul id="mobileSlider" class="fixed top-0 left-0 h-full w-3/4 bg-gray-900 text-white hidden">
-            <li>
-                <a href="">
-                    Acasa
-                </a>
-            </li>
+        <ul id="mobileSlider" class="fixed z-40 top-0 left-0 h-full w-3/4 bg-gray-900 text-white hidden">
             <li>
                 <a href="">
                     Meniu
@@ -37,8 +34,39 @@
                     Contanct
                 </a>
             </li>
+            @auth
+                <li>
+                    <a href="/">
+                        Account
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Comenzi
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Rezervari
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Adrese
+                    </a>
+                </li>
+                <li>
+                    <form method="post" action="{{ route('logout') }}">
+                        @csrf
+                        <button>
+                            Logout
+                        </button>
+                    </form>
+                </li>
+            @endauth
         </ul>
     </nav>
+    {{-- desktop nav --}}
     <nav class=" h-10 bg-orange-600 px-4 hidden lg:flex lg:items-center lg:justify-between">
         <div>
         </div>
@@ -68,7 +96,8 @@
         </div>
 
         <a class="relative rounded bg-trueGray-800 p-2 shadow-md" href="/">
-            <div class="absolute -top-1 -right-3 bg-white text-orange-700 px-1.5 py-0.5 rounded-full text-xs font-semibold">
+            <div
+                class="absolute -top-1 -right-3 bg-white text-orange-700 px-1.5 py-0.5 rounded-full text-xs font-semibold">
                 2
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#FFFFFF">
@@ -89,6 +118,142 @@
             </span>
             <div id="registerCaller" class="cursor-pointer text-gray-600 hover:text-gray-100 px-1">
                 Inregistrare
+            </div>
+        </div>
+        <div id="authModal" class="fixed top-1/2 left-0 transform -translate-y-1/2 w-full md:left-1/2 md:-translate-x-1/2 md:w-3/4 lg:w-1/2 2xl:w-1/3 flex-col items-center justify-center z-30 hidden">
+            <div
+                class="relative w-full rounded-md shadow-md flex flex-col items-stretch justify-start md:flex-row md:justify-between overflow-auto">
+                <button id="closeAuthModal" class="absolute top-0 right-0 cursor-pointer p-2">
+                    <svg class="fill-current text-white md:text-black" xmlns="http://www.w3.org/2000/svg" height="24px"
+                        viewBox="0 0 24 24" width="24px">
+                        <path d="M0 0h24v24H0V0z" fill="none" />
+                        <path
+                            d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+                    </svg>
+                </button>
+                <div class="bg-trueGray-800 text-white p-4 md:rounded-l-md">
+                    <div class="font-semibold text-lg">
+                        Advantajes of being a member
+                    </div>
+                    <ul class="list-disc ml-6 font-medium text-sm md:text-base">
+                        <li class="my-1">
+                            Order history
+                        </li>
+                        <li class="my-1">
+                            Repeat order
+                        </li>
+                        <li class="my-1">
+                            Track order
+                        </li>
+                        <li class="my-1">
+                            Save addresses
+                        </li>
+                    </ul>
+                </div>
+                <form id="loginForm" method="POST" action="{{ route('login') }}"
+                    class="bg-white p-6 md:flex-1 md:rounded-r-md hidden">
+                    @csrf
+
+                    <!-- Email Address -->
+                    <div>
+                        <x-label for="email" :value="__('Email')" />
+
+                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                            required autofocus />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-label for="password" :value="__('Password')" />
+
+                        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="current-password" />
+                    </div>
+
+                    <!-- Remember Me -->
+                    <div class="block mt-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox"
+                                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                name="remember">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <div class="flex flex-col">
+                            @if (Route::has('password.request'))
+                                <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                                    href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                            @endif
+                        </div>
+
+                        <x-button class="ml-3">
+                            {{ __('Log in') }}
+                        </x-button>
+                    </div>
+                </form>
+
+                <form id="registerForm" method="POST" action="{{ route('register') }}"
+                    class="bg-white p-6 md:flex-1 md:rounded-r-md hidden">
+                    @csrf
+
+                    {{-- First name --}}
+                    <div>
+                        <x-label for="firstName" :value="__('First name')" />
+
+                        <x-input id="firstName" class="block mt-1 w-full" type="text" name="first_name"
+                            :value="old('first_name')" required autofocus />
+                    </div>
+
+                    <!-- Name -->
+                    <div class="mt-4">
+                        <x-label for="name" :value="__('Name')" />
+
+                        <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                            autofocus />
+                    </div>
+
+                    {{-- Phone Number --}}
+                    <div class="mt-4">
+                        <x-label for="phoneNumber" :value="__('Phone number')" />
+
+                        <x-input id="phoneNumber" class="block mt-1 w-full" type="text" name="phone_number"
+                            :value="old('phone_number')" required autofocus />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-label for="email" :value="__('Email')" />
+
+                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                            required />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-label for="password" :value="__('Password')" />
+
+                        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="new-password" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                        <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                            name="password_confirmation" required />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <x-button class="ml-4">
+                            {{ __('Register') }}
+                        </x-button>
+                    </div>
+                </form>
             </div>
         </div>
     @endguest
@@ -124,10 +289,7 @@
                 </form>
             </li>
         </ul>
-
-
     @endauth
-
 </header>
 
 @push('scripts')
@@ -146,15 +308,8 @@
 
     <script>
         const mobileMenuToggler = document.getElementById("mobileMenuToggler");
-        const backdrop = document.getElementById("backdrop");
         const mobileSlider = document.getElementById("mobileSlider");
 
-        backdrop.addEventListener("click", function() {
-            if (!backdrop.classList.contains('hidden') && !backdrop.classList.contains('hidden')) {
-                backdrop.classList.add('hidden');
-                mobileSlider.classList.add('hidden');
-            }
-        })
         mobileMenuToggler.addEventListener("click", function() {
             toggleMenu();
         })
@@ -170,8 +325,51 @@
         }
     </script>
 
-    <script>
-        const loginCaller = document.getElementById('loginCaller');
-        const registerCaller = document.getElementById('registerCaller');
-    </script>
+    @guest
+        <script>
+            const authModal = document.getElementById('authModal');
+
+            const closeAuthModal = document.getElementById('closeAuthModal');
+            const loginCaller = document.getElementById('loginCaller');
+            const registerCaller = document.getElementById('registerCaller');
+
+            const loginForm = document.getElementById('loginForm');
+            const registerForm = document.getElementById('registerForm')
+
+            loginCaller.addEventListener('click', function() {
+                if (backdrop.classList.contains('hidden') && authModal.classList.contains('hidden')) {
+                    backdrop.classList.remove('hidden');
+                    authModal.classList.remove('hidden');
+                    authModal.classList.add('flex');
+                    loginForm.classList.remove('hidden');
+
+                    if (!registerForm.classList.contains('hidden')) {
+                        registerForm.classList.add('hidden');
+                    }
+                }
+            })
+
+            registerCaller.addEventListener('click', function() {
+                if (backdrop.classList.contains('hidden') && authModal.classList.contains('hidden')) {
+                    backdrop.classList.remove('hidden');
+                    authModal.classList.remove('hidden');
+                    authModal.classList.add('flex');
+                    if (!loginForm.classList.contains('hidden')) {
+                        loginForm.classList.add('hidden');
+                    }
+                    registerForm.classList.remove('hidden')
+                }
+            })
+
+
+            closeAuthModal.addEventListener('click', function() {
+                if (!backdrop.classList.contains('hidden') && !authModal.classList.contains('hidden')) {
+                    backdrop.classList.add('hidden');
+                    authModal.classList.add('hidden');
+                    authModal.classList.remove('flex');
+                }
+            })
+        </script>
+    @endguest
+
 @endpush
--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -1,30 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-screen">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title ?? "Store" }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ $title ?? 'Store' }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@2.4.21/dist/css/themes/splide-default.min.css">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/client/app.js') }}" defer></script>	
-        <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@2.4.21/dist/js/splide.min.js"></script>	
-    </head>
-    <body class="h-full flex flex-col">
-        <div id="backdrop" class="fixed top-0 left-0 h-full w-full bg-black bg-opacity-75 hidden"></div>
-        @include('includes.header')
-        <main class="flex-1 bg-gray-900">
-            @yield('content')
-        </main>
-        @include('includes.footer')
-    </body>
-    @stack('scripts')
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@splidejs/splide@2.4.21/dist/css/themes/splide-default.min.css">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/client/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@2.4.21/dist/js/splide.min.js"></script>
+</head>
+
+<body class="h-full flex flex-col">
+    @include('includes.header')
+    <main class="flex-1 bg-gray-900">
+        @yield('content')
+    </main>
+    @include('includes.footer')
+</body>
+<script>
+
+</script>
+@stack('scripts')
+<script>
+    const backdrop = document.getElementById("backdrop");
+
+    backdrop.addEventListener("click", function() {
+        if (!backdrop.classList.contains('hidden')) {
+            backdrop.classList.add('hidden');
+
+
+            if (mobileSlider && !mobileSlider.classList.contains('hidden')) {
+                mobileSlider.classList.add('hidden');
+            }
+ 
+            if (authModal && !authModal.classList.contains('hidden')) {
+                authModal.classList.add('hidden');
+            }
+        }
+    })
+</script>
+
 </html>
