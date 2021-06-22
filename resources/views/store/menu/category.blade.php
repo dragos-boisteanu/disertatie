3f1043b08c918974540a897cd97e326b050c57c2
Dragos <dragos360@yahoo.com>
2021-06-22 19:02:08 +0300

    some design improvements

--- resources/views/store/menu/category.blade.php
+++ resources/views/store/menu/category.blade.php
@@ -10,7 +10,8 @@
                     <h1 class="font-semibold text-xl mb-2 text-orange-600">{{ $subCategory->name }}</h1>
                     <ul>
                         @foreach ($subCategory->subProducts as $product)
-                            <li class="mb-4 w-full grid grid-cols-3 gap-y-4 md:grid-cols-5">
+                            <li class="mb-4 w-full grid grid-cols-3 gap-y-4 md:grid-cols-5 product-form">
+
                                 <div class="col-span-3 md:col-span-2">
                                     <h2 class="text-gray-400 mb-1 justify-self-start self-center">
                                         {{ $product->name }} - {{ $product->weight }}{{ $product->unit->name }}
@@ -24,14 +25,14 @@
                                     </ul>
                                 </div>
                                 <div class="text-sm self-center px-4 md:px-8">
-                                    <input type="number" value="1" min-value="1"
-                                        class="w-full rounded-sm bg-white text-center ">
+                                    <input id="pq{{ $product->id }}" type="number" value="1" min-value="1"
+                                        class="w-full rounded-sm bg-white text-center product-quantity ">
                                 </div>
                                 <div class="text-sm text-gray-300 self-center justify-self-center">
                                     {{ $product->price }} Ron
                                 </div>
                                 <button id="{{ $product->id }}"
-                                    class="self-center py-1 px-2 text-sm rounded border border-gray-400 text-gray-400 hover:text-gray-200 hover:border-gray-200">
+                                    class="self-center py-1 px-2 text-sm rounded border border-gray-400 text-gray-400 hover:text-gray-200 hover:border-gray-200 product-add-to-cart">
                                     Add to cart
                                 </button>
                             </li>
@@ -42,3 +43,26 @@
         </ul>
     </x-menu>
 @endsection
+
+
+@push('scripts')
+    <script>
+        const productQuantities = Array.from(document.querySelectorAll('.product-quantity'))
+        const productAddToCartBtn = document.querySelectorAll('.product-add-to-cart')
+
+        productAddToCartBtn.forEach(btn => {
+            btn.addEventListener('click', function(e) {
+                const productQuantity = productQuantities.filter(quantity => quantity.id.substring(2) == btn
+                    .id)[0];
+
+                const product = {
+                    productId: btn.id,
+                    productQuantity: productQuantity.value
+                }
+
+                console.log(product)
+                // ajax call to add product into user cart
+            })
+        })
+    </script>
+@endpush
--- resources/views/store/menu/index.blade.php
+++ resources/views/store/menu/index.blade.php
@@ -4,7 +4,7 @@
     <x-menu>
         @include('includes.menu-categories')
         @include('includes.menu-categories-select')
-        <div class="w-full mt-4 text-coolGray-700 text-4xl flex items-center justify-center font-semibold">
+        <div class="w-full lg:w-2/5 mt-4 text-coolGray-700 text-4xl flex items-center justify-center font-semibold">
             Choose a category
         </div>
     </x-menu>
