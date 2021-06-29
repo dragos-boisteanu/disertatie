d81579ed439808a1231655749e4711aca817a1b2
Dragos <dragos360@yahoo.com>
2021-06-29 20:16:33 +0300

    fixed link to menu

--- resources/views/store/cart.blade.php
+++ resources/views/store/cart.blade.php
@@ -25,7 +25,7 @@
     {!! Toastr::message() !!}
     <main id="store-app" class="flex-1 bg-gray-800 px-4 py-8 w-full flex items-center justify-center">
         <div class="h-full w-full  md:w-2/3 lg:w-1/4">
-            <a href=" {{ url()->previous() }}" class="text-sm text-orange-600 flex items-center hover:underline">
+            <a href=" {{ route('menu.index') }}" class="text-sm text-orange-600 flex items-center hover:underline">
                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24"
                     width="18px">
                     <path d="M0 0h24v24H0V0z" fill="none" />
