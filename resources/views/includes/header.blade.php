3392a0bca7082ca9ff3394e9dc5014c4b964b945
dragos720 <159753852456Gh>
2021-07-04 22:50:31 +0300

    menu route selected

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -81,35 +81,36 @@
 
     @endguest
 
+    
     @auth
         <ul class="w-full flex items-center justify-end gap-x-2 px-2 py-2 text-sm">
             <li>
                 <a href="{{ route('account.index') }}"
-                    class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1">
-                    Account
+                    class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1 {{ (request()->is('account*'))  ? '!text-gray-100' : '' }}">
+                    Cont
                 </a>
             </li>
             <li>
-                <a href="/" class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1">
-                    Orders
+                <a href="/" class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1 {{ (request()->is('orders*')) ? '!text-gray-100' : '' }}">
+                    Comenzi
                 </a>
             </li>
             <li>
-                <a href="/" class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1">
-                    Reservations
+                <a href="/" class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1 {{ (request()->is('reservations*')) ? '!text-gray-100' : '' }}">
+                    Reservari
                 </a>
             </li>
             <li>
                 <a href="{{ route('address.index') }}"
-                    class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1">
-                    Addresses
+                    class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1 {{ (request()->is('addresses*')) ? '!text-gray-100' : '' }}">
+                    Adrese
                 </a>
             </li>
             <li class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1">
                 <form method="post" action="{{ route('logout') }}">
                     @csrf
                     <button>
-                        Logout
+                        Delogare
                     </button>
                 </form>
             </li>
