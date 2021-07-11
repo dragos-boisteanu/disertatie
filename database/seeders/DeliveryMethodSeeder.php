39adbcf040d2a94f1fb791e0be1e08a7be9e0136
dragos720 <159753852456Gh>
2021-07-11 14:34:40 +0300

    chanbged delivery methods name in seeder

--- database/seeders/DeliveryMethodSeeder.php
+++ database/seeders/DeliveryMethodSeeder.php
@@ -15,17 +15,17 @@ class DeliveryMethodSeeder extends Seeder
     public function run()
     {
         DB::table('delivery_methods')->insert([
-            'name' => 'Delivery',
+            'name' => 'Livrarea la adresa',
             'price' => 15
         ]);
 
         DB::table('delivery_methods')->insert([
-            'name' => 'Local',
+            'name' => 'Ridicare din local',
             'price' => 0
         ]);
 
         DB::table('delivery_methods')->insert([
-            'name' => 'Table',
+            'name' => 'Comanda la masa',
             'price' => 0
         ]);
     }
