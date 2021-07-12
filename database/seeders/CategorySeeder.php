af491ddce7a4590940fb1ba05c114db5c38bef32
dragos720 <159753852456Gh>
2021-07-12 12:31:16 +0300

    position column value for categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,31 +18,36 @@ class CategorySeeder extends Seeder
         DB::table('categories')->insert([
             'name' => 'Pizza',
             'slug' => 'pizza',
-            'vat' => 9
+            'vat' => 9,
+            'position' => 1
         ]);
 
         DB::table('categories')->insert([
             'name' => 'Deserturi',
             'slug' => 'deserturi',
-            'vat' => 19
+            'vat' => 19,
+            'position' => 2
         ]);
 
         DB::table('categories')->insert([
             'name' => 'Ceaiuri',
             'slug' => 'ceaiuri',
-            'vat' => 19
+            'vat' => 19,
+            'position' => 3
         ]);
 
         DB::table('categories')->insert([
             'name' => 'Bere',
             'slug' => 'bere',
-            'vat' => 19
+            'vat' => 19,
+            'position' => 4
         ]);
 
         DB::table('categories')->insert([
             'name' => 'Vinuri',
             'slug' => 'vin',
-            'vat' => 19
+            'vat' => 19,
+            'position' => 5
         ]);
     }
 }
