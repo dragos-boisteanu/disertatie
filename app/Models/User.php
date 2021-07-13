2a7cb0ab44bc151d19ea3dd19846ca342669dd8b
Dragos <dragos360@yahoo.com>
2021-07-14 00:45:10 +0300

    remove eager loaded user role, cart, addresses

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,9 +50,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public $with = ['role', 'addresses', 'cart'];
-
+    
     protected $appends = array('fullName');
 
     public function getFullNameAttribute()
