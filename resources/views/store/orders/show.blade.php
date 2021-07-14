5a10d8e9d27da567c55704d64412afebd739111c
Dragos <dragos360@yahoo.com>
2021-07-14 15:22:39 +0300

    better animations for  order status

--- resources/views/store/orders/show.blade.php
+++ resources/views/store/orders/show.blade.php
@@ -13,72 +13,63 @@
 
         <div class="flex flex-wrap items-center justify-between">
             <h1 class="text-4xl my-4 pl-2 font-semibold text-trueGray-300 ">Comanda #{{ $order->id }} </h1>
-            @if ($order->status->name == 'Canceled')
-                <div id="canceledStatus" class="text-xs py-1 px-2 rounded-sm font-semibold bg-red-300
-                text-red-900">
-                    Anulata
-                </div>
-            @endif
+            <div id="finalStatus" class="text-xs py-1 px-2 rounded-sm font-semibold " style="display: none">
+                Anulata
+            </div>
         </div>
 
-        @if ($order->status->name != 'Canceled')
-            <div>
-                <h2 class="text-lg text-trueGray-300 my-4 pl-2">Statut comanda</h2>
-                <ul class="flex flex-col md:flex-row md:w-full">
-                    <li class="relative h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
-                        <div class="h-full w-1 bg-trueGray-100 md:h-1 md:w-full"></div>
-                        <div class="md:pr-1">
-                            <div class="flex-grow-0 text-xs py-1 px-2 rounded-sm font-semibold bg-sky-200 text-sky-800">
-                                Primita
-                            </div>
-                        </div>
-                    </li>
-                    <li class="h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
-                        <div id="isPreparingLine"
-                            class="h-full w-1 transition-colors !duration-700 bg-trueGray-700 md:h-1 md:w-full"></div>
-                        <div class="md:pr-1">
-                            <div id="isPreparing"
-                                class="text-xs py-1 px-2 rounded-sm transition-colors !duration-700 font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
-                                Se pregateste
-                            </div>
-                        </div>
-                    </li>
-                    {{-- bg-orange-200 text-orange-800 --}}
-                    <li class="h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
-                        <div id="inDeliveryLine"
-                            class="h-full w-1 transition-colors !duration-700 bg-trueGray-700 md:h-1 md:w-full"></div>
-                        <div class="md:pr-1">
-                            <div id="inDelivery"
-                                class="text-xs py-1 px-2 rounded-sm transition-colors !duration-700 font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
-                                Se livreaza
-                            </div>
-                        </div>
-                    </li>
-                    {{-- bg-sky-200 text-sky-800 --}}
-                    <li class="h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
-                        <div id="deliveredLine"
-                            class="h-full w-1 transition-colors !duration-700 bg-trueGray-700 md:h-1 md:w-full"></div>
-                        <div class="md:pr-1">
-                            <div id="delivered"
-                                class="text-xs py-1 px-2 rounded-sm transition-colors !duration-700 font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
-                                Livrata
-                            </div>
-                        </div>
-                    </li>
-                    {{-- bg-yellow-200 text-yellow-800 --}}
-                    <li class="h-6 flex items-start gap-x-2 md:w-1/5 md:h-14 md:flex-col md:gap-x-0 md:gap-y-2">
-                        <div id="completedLine"
-                            class="h-full w-1 transition-colors !duration-700 bg-trueGray-700 md:h-1 md:w-full"></div>
-                        <div id="completed"
-                            class="text-xs py-1 px-2 rounded-sm transition-colors !duration-700 font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
-                            Finalizata
-                        </div>
-                    </li>
-                    {{-- bg-green-200 text-green-800 --}}
-                </ul>
-            </div>
-        @endif
 
+        <div id="statusTracker">
+            <h2 class="text-lg text-trueGray-300 my-4 pl-2">Statut comanda</h2>
+            <ul class="flex flex-col md:flex-row md:w-full">
+                <li class="relative h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
+                    <div class="h-full w-1 bg-trueGray-100 md:h-1 md:w-full"></div>
+                    <div class="md:pr-1">
+                        <div class="flex-grow-0 text-xs py-1 px-2 rounded-sm font-semibold bg-sky-200 text-sky-800">
+                            Primita
+                        </div>
+                    </div>
+                </li>
+                <li class="h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
+                    <div id="isPreparingLine" class="h-full w-1bg-trueGray-700 md:h-1 md:w-full"></div>
+                    <div class="md:pr-1">
+                        <div id="isPreparing"
+                            class="text-xs py-1 px-2 rounded-sm font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
+                            Se pregateste
+                        </div>
+                    </div>
+                </li>
+                {{-- bg-orange-200 text-orange-800 --}}
+                <li class="h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
+                    <div id="inDeliveryLine" class="h-full w-1 bg-trueGray-700 md:h-1 md:w-full"></div>
+                    <div class="md:pr-1">
+                        <div id="inDelivery"
+                            class="text-xs py-1 px-2 rounded-sm  font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
+                            Se livreaza
+                        </div>
+                    </div>
+                </li>
+                {{-- bg-sky-200 text-sky-800 --}}
+                <li class="h-14 flex items-start gap-x-2 md:w-1/5 md:flex-col md:gap-x-0 md:gap-y-2">
+                    <div id="deliveredLine" class="h-full w-1 bg-trueGray-700 md:h-1 md:w-full"></div>
+                    <div class="md:pr-1">
+                        <div id="delivered"
+                            class="text-xs py-1 px-2 rounded-sm font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
+                            Livrata
+                        </div>
+                    </div>
+                </li>
+                {{-- bg-yellow-200 text-yellow-800 --}}
+                <li class="h-6 flex items-start gap-x-2 md:w-1/5 md:h-14 md:flex-col md:gap-x-0 md:gap-y-2">
+                    <div id="completedLine" class="h-full w-1 bg-trueGray-700 md:h-1 md:w-full"></div>
+                    <div id="completed"
+                        class="text-xs py-1 px-2 rounded-sm font-semibold text-trueGray-700 bg-transparent border border-trueGray-700">
+                        Finalizata
+                    </div>
+                </li>
+                {{-- bg-green-200 text-green-800 --}}
+            </ul>
+        </div>
 
         <div>
             <h2 class="text-lg text-trueGray-300 my-4 pl-2">Detalii comanda</h2>
@@ -123,7 +114,6 @@
                         <pre>{{ $order->observations }}</pre>
                     </div>
                 @endisset
-
             </div>
         </div>
 
@@ -214,36 +204,142 @@
 @push('scripts')
 
     <script>
-        window.Echo.private('orders.{{ $order->id }}').listen("OrderStatusUpdate", (e) => {
-            const status = e.status.toLowerCase().replace(/\s/g, '');
+        const status = "{{ $order->status->name }}";
+
+        const finalStatus = $('#finalStatus');
+        const statusTracker = $('#statusTracker');
+
+        const statusUpdate = function(status, animate) {
+            const isPreparingLine = $('#isPreparingLine');
+            const isPreparing = $('#isPreparing');
+
+            const inDeliveryLine = $('#inDeliveryLine');
+            const inDelivery = $('#inDelivery');
+
+            const deliveredLine = $('#deliveredLine');
+            const delivered = $('#delivered');
 
             switch (status) {
                 case 'ispreparing':
-                    $('#isPreparingLine').removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
-                    $('#isPreparing').removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                    if (animate) {
+                        isPreparingLine.addClass('transition-colors !duration-700');
+                        isPreparing.addClass('transition-colors !duration-700');
+                    }
+
+                    isPreparingLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    isPreparing.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
                         .addClass('bg-orange-200 text-orange-800');
                     break;
 
                 case 'indelivery':
-                    $('#inDeliveryLine').removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
-                    $('#inDelivery').removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                    if (animate) {
+                        isPreparingLine.addClass('transition-colors !duration-700');
+                        isPreparing.addClass('transition-colors !duration-700');
+
+                        inDeliveryLine.addClass('transition-colors !duration-700');
+                        inDelivery.addClass('transition-colors !duration-700');
+                    }
+
+                    isPreparingLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    isPreparing.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                        .addClass('bg-orange-200 text-orange-800');
+
+                    inDeliveryLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    inDelivery.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
                         .addClass('bg-yellow-200 text-yellow-800');
                     break;
 
                 case 'delivered':
-                    $('#deliveredLine').removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
-                    $('#delivered').removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                    if (animate) {
+                        isPreparingLine.addClass('transition-colors !duration-700');
+                        isPreparing.addClass('transition-colors !duration-700');
+
+                        inDeliveryLine.addClass('transition-colors !duration-700');
+                        inDelivery.addClass('transition-colors !duration-700');
+
+                        deliveredLine.addClass('transition-colors !duration-700');
+                        delivered.addClass('transition-colors !duration-700');
+                    }
+
+                    isPreparingLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    isPreparing.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                        .addClass('bg-orange-200 text-orange-800');
+
+                    inDeliveryLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    inDelivery.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                        .addClass('bg-yellow-200 text-yellow-800');
+
+                    deliveredLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    delivered.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
                         .addClass('bg-teal-200 text-team-800');
                     break;
 
                 case 'completed':
-                    $('#completedLine').removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
-                    $('#completed').removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                    const completedLine = $('#completedLine');
+                    const completed = $('#completed');
+
+                    if (animate) {
+                        isPreparingLine.addClass('transition-colors !duration-700');
+                        isPreparing.addClass('transition-colors !duration-700');
+
+                        inDeliveryLine.addClass('transition-colors !duration-700');
+                        inDelivery.addClass('transition-colors !duration-700');
+
+                        deliveredLine.addClass('transition-colors !duration-700');
+                        delivered.addClass('transition-colors !duration-700');
+
+                        completedLine.addClass('transition-colors !duration-700');
+                        completed.addClass('transition-colors !duration-700');
+                    }
+
+                    isPreparingLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    isPreparing.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                        .addClass('bg-orange-200 text-orange-800');
+
+                    inDeliveryLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    inDelivery.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                        .addClass('bg-yellow-200 text-yellow-800');
+
+                    deliveredLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    delivered.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
+                        .addClass('bg-teal-200 text-team-800');
+
+                    completedLine.removeClass('bg-trueGray-700').addClass('bg-trueGray-100');
+                    completed.removeClass('text-trueGray-700 bg-transparent border border-trueGray-700')
                         .addClass('bg-green-200 text-green-800');
                     break;
-            }
+                case 'canceled':
+                    finalStatus.addClass('bg-red-300 text-red-900');
+                    finalStatus.fadeIn();
+                    statusTracker.slideUp();
 
-        });
+                    break;
+            }
+        }
+
+        if (status == 'Canceled' || status == 'Completed') {
+
+            finalStatus.show();
+            statusTracker.hide();
+
+            if (status == 'Canceled') {
+                finalStatus.addClass('bg-red-300 text-red-900');
+            } else {
+                finalStatus.addClass('bg-green-300 text-green-900');
+            }
+        } else {
+
+            finalStatus.hide();
+            statusTracker.show();
+
+            statusUpdate(status.toLowerCase().replace(/\s/g, ''), false);
+
+            window.Echo.private('orders.{{ $order->id }}').listen("OrderStatusUpdate", (e) => {
+                const status = e.status.toLowerCase().replace(/\s/g, '');
+                statusUpdate(status, true);
+
+            });
+        }
     </script>
 
 @endpush
