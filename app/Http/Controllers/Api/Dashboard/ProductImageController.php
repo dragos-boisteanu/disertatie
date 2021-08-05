8c62fd2ea59baa62275162f13dc3e40df6cdf86c
Dragos <dragos360@yahoo.com>
2021-08-05 14:29:54 +0300

    image upload and delete service

--- app/Http/Controllers/Api/Dashboard/ProductImageController.php
+++ app/Http/Controllers/Api/Dashboard/ProductImageController.php
@@ -6,25 +6,28 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
+use App\Interfaces\ImageServiceInterface;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ProductImageController extends Controller
 {
+
+	private $imageService;
+
+	public function __construct(ImageServiceInterface $imageService)
+	{
+		$this->imageService = $imageService;
+	}
+
 	public function store(Request $request, $id)
 	{
 		try {
 			$storagePath = '/products_images' . '/' . $id;
-			$files =  Storage::allFiles($storagePath);
-			Storage::delete($files);
 
-			$path = $request->file('image')->store($storagePath);
-			$path = '/storage/' . $path;
-
-			DB::table('products')->where('id', $id)->update(['image' => $path]);
+			$path = $this->imageService->storeImage($request->file('image'), $storagePath, 'products', 'image', $id);
 
 			return response()->json(['imagePath' => $path], 200);
 		} catch (\Exception $e) {
-			debug($e);
 			return response()->json(['message' => $e->getTrace()], 500);
 		}
 	}
@@ -32,27 +35,12 @@ class ProductImageController extends Controller
 	public function destroy($id)
 	{
 		try {
-			DB::beginTransaction();
-
-			$product = DB::table('products')->select('image')->where('id', $id)->first();
-
-			if (!isset($product)) {
-				throw new ModelNotFoundException();
-			}
-
-			DB::table('products')->where('id', $id)->update(['image' => null]);
-			Storage::delete($product->image);
-
-			DB::commit();
+			$this->imageService->deleteImage('products', 'image', $id);
 
 			return response()->json(['message' => 'Image removed'], 200);
 		} catch (ModelNotFoundException $e) {
-			DB::rollBack();
-			debug($e);
 			return response()->json(['message' => 'Product not found'], 500);
 		} catch (\Exception $e) {
-			DB::rollBack();
-			debug($e);
 			return response()->json(['message' => 'Something went wrong, try again later'], 500);
 		}
 	}
---
+++ app/Services/ImageService.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace App\Services;
+
+use Exception;
+use App\Models\Cart;
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use App\Interfaces\CartServiceInterface;
+use App\Interfaces\ImageServiceInterface;
+use Barryvdh\Debugbar\Facade as Debugbar;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class ImageService implements ImageServiceInterface
+{
+
+	public function storeImage(UploadedFile $image, string $storagePath, string $table, string $column, int $id): string
+	{
+		try {
+			$files =  Storage::allFiles($storagePath);
+			Storage::delete($files);
+
+			$path = $image->store($storagePath);
+			$path = '/storage/' . $path;
+
+			DB::table($table)->where('id', $id)->update([$column => $path]);
+
+			return $path;
+		} catch (\Exception $e) {
+			throw new Exception('Something went wrong, try again later');
+		}
+	}
+
+	public function deleteImage(string $table, string $column, int $id): void
+	{
+		try {
+			DB::beginTransaction();
+
+
+			$model = DB::table($table)->select($column)->where('id', $id)->first();
+
+			if (!isset($model)) {
+				throw new ModelNotFoundException();
+			}
+
+			DB::table($table)->where('id', $id)->update([$column => null]);
+			Storage::delete($model->$column);
+
+			DB::commit();
+		} catch (ModelNotFoundException $e) {
+			DB::rollBack();
+			throw new ModelNotFoundException('Product not found');
+		} catch (\Exception $e) {
+			DB::rollBack();
+			throw new ModelNotFoundException('Something went wrong, try again later');
+		}
+	}
+}
