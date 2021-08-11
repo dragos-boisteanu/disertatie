a5018db01829e4a1d2c4a6d8255c824bb328e536
Dragos <dragos360@yahoo.com>
2021-08-11 18:23:37 +0300

    categories menu sticky

--- resources/views/includes/menu-categories.blade.php
+++ resources/views/includes/menu-categories.blade.php
@@ -1,4 +1,4 @@
-<ul class="text-gray-600 w-28 hidden lg:block">
+<ul class="sticky top-8 text-gray-600 w-28 hidden lg:block">
 	@foreach ($categories as $category)
 	<li
 		class="text-center text-lg mt-2 pb-2 w-full  border-b border-transparent hover:text-gray-300 hover:border-gray-300 @if (Route::current()->parameter('categorySlug') == $category->slug) !text-gray-300 !border-gray-300 @endif">
