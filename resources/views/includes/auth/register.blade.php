88abf3bd54183bd81ce26b527929392e50acabde
Dragos <dragos360@yahoo.com>
2021-08-16 00:23:09 +0300

    mobile slider menu

--- resources/views/includes/auth/register.blade.php
+++ resources/views/includes/auth/register.blade.php
@@ -1,74 +1,67 @@
 <form id="registerForm" class="modal" autocomplete="off">
-    @csrf
+	@csrf
 
-    <div>
-        <x-label for="firstName" :value="__('First name')" />
+	<div>
+		<x-label for="firstName" :value="__('First name')" />
 
-        <x-input id="firstName" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')"
-            required autofocus />
-        <div id="firstNameError" class="font-medium text-red-600"></div>
-    </div>
+		<x-input id="firstName" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required
+			autofocus />
+		<div id="firstNameError" class="font-medium text-red-600"></div>
+	</div>
 
-    <div class="mt-4">
-        <x-label for="lastName" :value="__('Last name')" />
+	<div class="mt-4">
+		<x-label for="lastName" :value="__('Last name')" />
 
-        <x-input id="lastName" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required
-            autofocus />
-        <div id="lastNameError" class="font-medium text-red-600"></div>
-    </div>
+		<x-input id="lastName" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required
+			autofocus />
+		<div id="lastNameError" class="font-medium text-red-600"></div>
+	</div>
 
-    <div class="mt-4">
-        <x-label for="phoneNumber" :value="__('Phone number')" />
+	<div class="mt-4">
+		<x-label for="phoneNumber" :value="__('Phone number')" />
 
-        <x-input id="phoneNumber" class="block mt-1 w-full" type="text" name="phone_number" :value="old('phone_number')"
-            required autofocus />
-        <div id="phoneNumberError" class="font-medium text-red-600"></div>
-    </div>
+		<x-input id="phoneNumber" class="block mt-1 w-full" type="text" name="phone_number" :value="old('phone_number')"
+			required autofocus />
+		<div id="phoneNumberError" class="font-medium text-red-600"></div>
+	</div>
 
-    <div class="mt-4">
-        <x-label for="email" :value="__('Email')" />
+	<div class="mt-4">
+		<x-label for="email" :value="__('Email')" />
 
-        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-        <div id="emailError" class="font-medium text-red-600"></div>
-    </div>
-
-
-    <div class="mt-4">
-        <x-label for="password" :value="__('Password')" />
-
-        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
-            autocomplete="new-password" />
-        <div id="passwordError" class="font-medium text-red-600"></div>
-    </div>
+		<x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+		<div id="emailError" class="font-medium text-red-600"></div>
+	</div>
 
 
-    <div class="mt-4">
-        <x-label for="password_confirmation" :value="__('Confirm Password')" />
+	<div class="mt-4">
+		<x-label for="password" :value="__('Password')" />
 
-        <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation"
-            required />
-        <div id="password_confirmationError" class="font-medium text-red-600"></div>
-    </div>
+		<x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+			autocomplete="new-password" />
+		<div id="passwordError" class="font-medium text-red-600"></div>
+	</div>
 
-    <div class="flex items-center justify-end mt-4">
-        <x-button class="ml-4">
-            {{ __('Register') }}
-        </x-button>
-    </div>
+
+	<div class="mt-4">
+		<x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+		<x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation"
+			required />
+		<div id="password_confirmationError" class="font-medium text-red-600"></div>
+	</div>
+
+	<div class="flex items-center justify-end mt-4">
+		<x-button class="ml-4">
+			{{ __('Register') }}
+		</x-button>
+	</div>
 </form>
 
 
 @push('scripts')
 
-    <script>
-        const registerForm = $('#registerForm');
-
-        $('#register').click(function() {
-            registerForm.modal({
-                fadeDuration: 150,
-                fadeDelay: 0.50
-            });
-        })
+<script>
+	const registerForm = $('#registerForm');
 
         registerForm.on($.modal.BEFORE_CLOSE, function(event, modal) {
             $('#registerForm *').filter(":input").not(':button').not('input[type=hidden]').each(function(index,
@@ -110,6 +103,6 @@
                 }
             })
         })
-    </script>
+</script>
 
 @endpush
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,27 +1,24 @@
 <header id="top" class="flex-none bg-gray-900">
+
+	<div id="backdrop" class="fixed top-0 left-0 z-10 bg-black bg-opacity-75 w-full h-screen" style="display: none"></div>
+
 	<div class="w-full h-52">
 		<img class="h-full w-full object-cover" src='{{ asset("/storage/header3.jpg")}}' />
 	</div>
-	{{-- mobile nav --}}
-	<nav class="bg-orange-600 flex items-center justify-between px-4 lg:hidden">
-		<ul class="flex items-center gap-x-4 py-4">
-			<li>
-				<a class="text-white text-sm px-2 py-1 rounded bg-trueGray-800 bg-opacity-9" href="/">
-					Meniu
-				</a>
 
-			</li>
-			<li>
-				<a class="text-white text-sm px-2 py-1 rounded bg-trueGray-800 bg-opacity-90" href="/">
-					Contanct
-				</a>
-			</li>
-			<li>
-				<a class="text-white text-sm px-2 py-1 rounded bg-trueGray-800 bg-opacity-90" href="/">
-					About us
-				</a>
-			</li>
-		</ul>
+	{{-- mobile nav --}}
+	<nav class="relative bg-orange-600 flex items-center justify-between px-4 py-1 lg:hidden">
+
+		<div id="mobileMenuBtn" class="cursor-pointer">
+			@for ($i = 0; $i < 3; $i++) <div class="bg-black w-6 h-0.5 my-1.5">
+		</div>
+		@endfor
+		</div>
+
+		<div class="absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 w-32 h-32">
+			<img class="h-full w-full rounded-full" src='{{ asset("/storage/logo.png") }}' />
+		</div>
+
 		<x-mobile-cart-button :cart="$cart"></x-mobile-cart-button>
 	</nav>
 
@@ -50,27 +47,109 @@
 		<x-desktop-cart :cart="$cart"></x-desktop-cart>
 	</nav>
 
-	@guest
-	<div class="w-full flex items-center justify-end px-2 py-2 text-sm">
-		<div id="auth" class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1">
-			Autentificare
+
+	{{-- mobile menu slider --}}
+	<div id="mobileSlider" class="fixed z-20 left-0 top-0 w-3/4 h-screen bg-trueGray-900 shadow-md flex flex-col"
+		style="display: none">
+
+		<div class="flex-initial w-full flex items-center justify-center border-b border-trueGray-400 py-4">
+			<div class="w-32 h-32">
+				<img class="h-full w-full rounded-full" src='{{ asset("/storage/logo.png") }}' />
+			</div>
 		</div>
-		<span class="text-red-700 px-1">
-			/
-		</span>
-		<div id="register" class="cursor-pointer text-gray-500 hover:text-gray-100 px-1">
-			Inregistrare
+
+		<ul class="flex-initial mt-4 w-full text-sm">
+			<li>
+				<a class="block py-2 px-4 border-transparent border-l-4 cursor-pointer outline-none text-gray-500 hover:text-gray-100 {{ request()->is('/') ? '!text-gray-100 border-white bg-trueGray-100 bg-opacity-10' : '' }}"
+					href="/">
+					Meniu
+				</a>
+			</li>
+			<li>
+				<a class="block py-2 px-4 border-transparent border-l-4 cursor-pointer outline-none text-gray-500 hover:text-gray-100 {{ request()->is('contact') ? '!text-gray-100 border-white bg-trueGray-100 bg-opacity-10' : '' }}"
+					href="/">
+					Contanct
+				</a>
+			</li>
+			<li>
+				<a class="block py-2 px-4 border-transparent border-l-4 cursor-pointer outline-none text-gray-500 hover:text-gray-100 {{ request()->is('about') ? '!text-gray-100 border-white bg-trueGray-100 bg-opacity-10' : '' }}"
+					href="/">
+					About us
+				</a>
+			</li>
+		</ul>
+		@auth
+		<ul class="flex-initial mt-4 w-full text-sm">
+			<li>
+				<a href="{{ route('account.index') }}"
+					class="block py-2 px-4 border-transparent border-l-4 cursor-pointer outline-none text-gray-500 hover:text-gray-100 {{ request()->is('account') ? '!text-gray-100 border-white bg-trueGray-100 bg-opacity-10' : '' }}">
+					Cont
+				</a>
+			</li>
+			<li>
+				<a href="{{ route('orders.index') }}"
+					class="block py-2 px-4 border-transparent border-l-4 cursor-pointer outline-none text-gray-500 hover:text-gray-100 {{ request()->is('account/orders*') ? '!text-gray-100 border-white bg-trueGray-100 bg-opacity-10' : '' }}">
+					Comenzi
+				</a>
+			</li>
+			<li>
+				<a href="{{ route('reservations.index') }}"
+					class="block py-2 px-4 border-transparent border-l-4 cursor-pointer outline-none text-gray-500 hover:text-gray-100 {{ request()->is('account/reservations*') ? '!text-gray-100 border-white bg-trueGray-100 bg-opacity-10' : '' }}">
+					Reservari
+				</a>
+			</li>
+			<li>
+				<a href="{{ route('address.index') }}"
+					class="block py-2 px-4 border-transparent border-l-4 cursor-pointer outline-none text-gray-500 hover:text-gray-100 {{ request()->is('addresses*') ? '!text-gray-100 border-white bg-trueGray-100 bg-opacity-10' : '' }}">
+					Adrese
+				</a>
+			</li>
+		</ul>
+		<div class="flex-1 h-full flex items-end justify-start">
+			<form method="post" action="{{ route('logout') }}" class="w-full border-t border-trueGray-400 p-4">
+				@csrf
+				<button
+					class="group w-full flex items-center justify-center gap-x-4 cursor-pointer outline-none text-gray-500 px-1">
+					<svg class="text-red-600 group-hover:text-red-500 fill-current" xmlns="http://www.w3.org/2000/svg"
+						enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px">
+						<g>
+							<path d="M0,0h24v24H0V0z" fill="none" />
+						</g>
+						<g>
+							<path
+								d="M17,8l-1.41,1.41L17.17,11H9v2h8.17l-1.58,1.58L17,16l4-4L17,8z M5,5h7V3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h7v-2H5V5z" />
+						</g>
+					</svg>
+					<span class="group-hover:text-gray-100">Delogare</span>
+				</button>
+			</form>
 		</div>
+		@endauth
+		@guest
+		<div class="flex-1 h-full flex items-end justify-start">
+			<div
+				class="w-full border-t border-trueGray-400 p-4 flex items-center justify-center gap-x-4 cursor-pointer outline-none text-gray-500 px-1">
+				<div id="auth" class="cursor-pointer outline-none hover:text-gray-100 px-1">
+					Autentificare
+				</div>
+				<span class="text-red-700 px-1">
+					/
+				</span>
+				<div id="register" class="cursor-pointer hover:text-gray-100 px-1">
+					Inregistrare
+				</div>
+			</div>
+
+			@include('includes.auth.login')
+			@include('includes.auth.register')
+		</div>
+		@endguest
 	</div>
 
-	@include('includes.auth.login')
-	@include('includes.auth.register')
-
-	@endguest
-
-
 	@auth
-	<ul class="w-full flex items-center justify-end gap-x-2 px-2 py-2 text-sm">
+	<div class="h-9 lg:hidden"></div>
+	{{-- desktop menu --}}
+	<ul class="hidden w-full items-center justify-end gap-x-2 px-2 py-2 text-sm lg:flex">
 		<li>
 			<a href="{{ route('account.index') }}"
 				class="cursor-pointer outline-none text-gray-500 hover:text-gray-100 px-1 {{ request()->is('account') ? '!text-gray-100' : '' }}">
@@ -123,6 +202,39 @@
         }).mount();
     </script> --}}
 
+<script>
+	const mobileSlider = $('#mobileSlider');
+	const backdrop = $('#backdrop');
+
+	$('#auth').click(function() {
+		backdrop.hide();
+		mobileSlider.hide();
+		loginForm.modal({
+			fadeDuration: 150,
+			fadeDelay: 0.50
+		});
+	})
+
+	$('#register').click(function() {
+		backdrop.hide();
+		mobileSlider.hide();
+		registerForm.modal({
+			fadeDuration: 150,
+			fadeDelay: 0.50
+			});
+	})
+
+	backdrop.click(function() {
+		mobileSlider.hide();
+		backdrop.hide();
+	})
+
+
+	$('#mobileMenuBtn').click(function() {
+		backdrop.show();
+		mobileSlider.show();
+	});
+</script>
 
 
 @endpush
