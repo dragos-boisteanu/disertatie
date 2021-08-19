2c79488f4752bf7a2b3fa4a3362e8cce76afc161
Dragos <dragos360@yahoo.com>
2021-08-19 16:31:40 +0300

    removed unecesary if from products filters

--- app/Filters/Product/CategoryFilter.php
+++ app/Filters/Product/CategoryFilter.php
@@ -3,14 +3,10 @@
 
 namespace App\Filters\Product;
 
-class CategoryFilter 
+class CategoryFilter
 {
-    public function filter($builder, $value)
-    {
-        if(is_array($value)) {
-            return $builder->whereIn('category_id', $value);
-        }
-        
-        return $builder->where('category_id', $value);
-    }
+	public function filter($builder, $value)
+	{
+		return $builder->whereIn('category_id', $value);
+	}
 }
