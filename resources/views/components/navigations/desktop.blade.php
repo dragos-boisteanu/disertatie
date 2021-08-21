cf42dd5260a91899dc9aaea977e8192c2e26934f
dragos720 <159753852456Gh>
2021-08-22 00:44:41 +0300

    center logo on desktop nav bar

--- resources/views/components/navigations/desktop.blade.php
+++ resources/views/components/navigations/desktop.blade.php
@@ -1,15 +1,15 @@
 @props(['cart'])
 
 <nav class=" bg-orange-600 py-2 px-4 hidden lg:flex lg:items-center lg:justify-between">
-	<div class="w-8 px-1"></div>
-	<ul class=" w-full flex items-start justify-center px-4 gap-x-20">
-		<li>
+	<div class="w-9"></div>
+	<ul class=" w-full flex items-start justify-center gap-x-20">
+		<li class="w-24">
 			<a class="text-white text-lg pb-0.5 border-b-2 border-orange-600 hover:border-black hover:text-black  {{ request()->is('menu/*') ? '!border-black !text-black ' : '' }}"
 				href="{{ route('home') }}">
 				Acasa
 			</a>
 		</li>
-		<li>
+		<li class="w-24">
 			<a class="text-white text-lg pb-0.5 border-b-2 border-orange-600 hover:border-black hover:text-black  {{ request()->is('menu/*') ? '!border-black !text-black ' : '' }}"
 				href="{{ route('menu.index') }}">
 				Meniu
@@ -22,13 +22,13 @@
 			</a>
 		</li>
 
-		<li>
+		<li class="w-24">
 			<a class="text-white text-lg pb-0.5 border-b-2 border-orange-600 hover:border-black hover:text-black {{ request()->is('about') ? '!border-black !text-black ' : '' }}"
 				href="/">
 				Despre noi
 			</a>
 		</li>
-		<li>
+		<li class="w-24">
 			<a class="text-white text-lg pb-0.5 border-b-2 border-orange-600 hover:border-black hover:text-black {{ request()->is('contanct') ? '!border-black !text-black ' : '' }}"
 				href="{{ route('contact') }}">
 				Contanct
