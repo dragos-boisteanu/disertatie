1781e26d8c9fbf39e57bf6cd74dc859dd66c542e
Dragos <dragos360@yahoo.com>
2021-08-23 13:29:02 +0300

    changed migration columns order

--- database/migrations/2021_08_22_130354_add_image_column_to_categories_table.php
+++ database/migrations/2021_08_22_130354_add_image_column_to_categories_table.php
@@ -14,7 +14,7 @@ class AddImageColumnToCategoriesTable extends Migration
 	public function up()
 	{
 		Schema::table('categories', function (Blueprint $table) {
-			$table->string('image')->nullable()->before('id');
+			$table->string('image')->nullable()->before('name');
 		});
 	}
 
