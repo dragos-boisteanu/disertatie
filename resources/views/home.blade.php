1c9c0e7b4635654365025510592da70d4d6be620
Dragos <dragos360@yahoo.com>
2021-08-23 21:52:30 +0300

    fix categories image display size

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,17 @@
 @extends('layouts.home')
 
-
 @section('content')
 <div class="w-full mx-auto mt-8 lg:w-11/12 2xl:w-5/6">
 	<ul class="w-full h-full gap-6 md:grid md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
 		@foreach($categories as $category)
 		<li
 			class="bg-white mt-6 rounded transition-all duration-700 ease-in-out hover:-translate-y-3 md:mt-0 hover:shadow-lg">
-			<a class="block w-full h-full p-3 " href="{{ route('category.show', ['categorySlug'=>$category->slug])}}">
+			<a class="block w-full h-full p-3" href="{{ route('category.show', ['categorySlug'=>$category->slug])}}">
 				<div class="h-80 w-full">
 					@isset($category->image)
 					<img class="h-full w-full rounded bg-cover" src="{{ asset("$category->image")}}" alt="category image" />
 					@else
-					<img class="h-full w-full rounded bg-cover"
+					<img class="h-full w-full rounded bg-fill"
 						src="https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg" alt="category image" />
 					@endif
 
