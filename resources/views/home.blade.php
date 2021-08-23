693b92ccdf96660f255fd89f6e87830ce2541fa7
Dragos <dragos360@yahoo.com>
2021-08-23 23:13:01 +0300

    to top improvements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="w-full mx-auto mt-8 lg:w-11/12 2xl:w-5/6">
-	<ul class="w-full h-full gap-6 md:grid md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
+	<ul class="mx-auto w-5/6 h-full gap-6 md:grid md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
 		@foreach($categories as $category)
 		<li
 			class="bg-white mt-6 rounded transition-all duration-700 ease-in-out hover:-translate-y-3 md:mt-0 hover:shadow-lg">
-			<a class="block w-full h-full p-3" href="{{ route('category.show', ['categorySlug'=>$category->slug])}}">
+			<a class="block w-full p-3" href="{{ route('category.show', ['categorySlug'=>$category->slug])}}">
 				<div class="h-80 w-full">
 					@isset($category->image)
 					<img class="h-full w-full rounded object-cover" src="{{ asset("$category->image")}}" alt="category image" />
