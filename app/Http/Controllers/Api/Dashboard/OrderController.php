c9e71de75781ced3e86e0495174347059173a757
Dragos <dragos360@yahoo.com>
2021-08-24 13:56:50 +0300

    fixed bug related linking staff with order

--- app/Http/Controllers/Api/Dashboard/OrderController.php
+++ app/Http/Controllers/Api/Dashboard/OrderController.php
@@ -20,121 +20,118 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class OrderController extends Controller
 {
-    private $orderService;
+	private $orderService;
 
-    public function __construct(OrderServiceInterface $orderService)
-    {
-        $this->orderService = $orderService;
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        try {
-            $orders = $this->orderService->getOrders(5, $request->orderBy, $request->all());
-            return new OrderListCollection($orders);
-        } catch ( \Exception $ex )  {
-            return  response()->json(['message'=>'Something when wrong'], 500);
-            // return  response()->json([ $ex->getMessage()], 500);
-        }
-    }
+	public function __construct(OrderServiceInterface $orderService)
+	{
+		$this->orderService = $orderService;
+	}
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+		try {
+			$orders = $this->orderService->getOrders(5, $request->orderBy, $request->all());
+			return new OrderListCollection($orders);
+		} catch (\Exception $ex) {
+			return  response()->json(['message' => 'Something when wrong'], 500);
+			// return  response()->json([ $ex->getMessage()], 500);
+		}
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(OrderStoreRequest $request)
-    {
-        try  {
-            $this->orderService->create($request->validated(), $request->user()->id);
-        
-            return response()->json(['message'=>'Order created succesfully'], 201 );
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(OrderStoreRequest $request)
+	{
+		try {
+			$this->orderService->create($request->validated(), $request->user()->id);
 
-        } catch (\Exception $e ) {
-            DB::rollBack();
-            return  response()->json(['message'=>$e->getMessage()], 400 );
-        };
-    }
+			return response()->json(['message' => 'Order created succesfully'], 201);
+		} catch (\Exception $e) {
+			DB::rollBack();
+			return  response()->json(['message' => $e->getMessage()], 400);
+		};
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        try {
-            $order = $this->orderService->getOrderById($id);
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		try {
+			$order = $this->orderService->getOrderById($id);
 
-            if(Auth::check() && Auth::user()->isWaiter && is_null($order->staff_id)) {
-                $order = $this->orderService->linkWaiterWithOrder(Auth::id(), $order);
-                $order->load('staff');
-            }
+			if ((Auth::user()->isWaiter() || Auth::user()->isLocationManager() || Auth::user()->isAdminitrator()) && is_null($order->staff_id)) {
+				$order = $this->orderService->linkWaiterWithOrder(Auth::id(), $order);
+				$order->load('staff');
+			}
 
-            debug($order);
+			return new OrderResource($order);
+		} catch (ModelNotFoundException $me) {
+			return  response()->json(['message' => $me->getMessage()], 404);
+		} catch (\Exception $e) {
+			return  response()->json(['message' => $e->getMessage()], 500);
+		}
+	}
 
-            return new OrderResource($order); 
-        } catch (ModelNotFoundException $me) {
-            return  response()->json(['message'=>$me->getMessage()], 404);
-        } catch (\Exception $e) {
-            return  response()->json(['message'=>$e->getMessage()], 500);
-        }
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(OrderPatchRequest $request, $id)
+	{
+		try {
+			$order = $this->orderService->update($request->validated(), $id);
+			return response()->json($order->updated_at, 200);
+		} catch (ModelNotFoundException $me) {
+			return  response()->json(['message' => $me->getMessage()], 404);
+		} catch (\Exception $e) {
+			return  response()->json(['message' => $e->getMessage()], 500);
+		}
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(OrderPatchRequest $request, $id)
-    {
-        try {
-            $order = $this->orderService->update($request->validated(), $id);
-            return response()->json($order->updated_at, 200);
-        } catch (ModelNotFoundException $me) {
-            return  response()->json(['message'=>$me->getMessage()], 404);
-        } catch (\Exception $e) {
-            return  response()->json(['message'=>$e->getMessage()], 500);
-        }
-    }
+	public function updateStatus(OrderPatchStatusRequest $request, $id)
+	{
+		try {
+			$order = $this->orderService->updateStatus($request->status['id'], $id);
 
-    public function updateStatus(OrderPatchStatusRequest $request, $id) 
-    {
-        try {
-            $order = $this->orderService->updateStatus($request->status['id'], $id);
+			return response()->json([
+				'updatedAt' => $order->updated_at,
+				'status' => $order->status
+			]);
+		} catch (ModelNotFoundException $me) {
+			return  response()->json(['message' => $me->getMessage()], 404);
+		} catch (\Exception $e) {
+			return  response()->json(['message' => $e->getMessage()], 500);
+		}
+	}
 
-            return response()->json([
-                'updatedAt' => $order->updated_at,
-                'status' => $order->status
-            ]);
-        } catch (ModelNotFoundException $me) {
-            return  response()->json(['message'=>$me->getMessage()], 404);
-        } catch (\Exception $e) {
-            return  response()->json(['message'=>$e->getMessage()], 500);
-        }
-    }
+	public function disable($id)
+	{
+		try {
+			$order = $this->orderService->disable($id);
 
-    public function disable($id) 
-    {
-        try {
-            $order = $this->orderService->disable($id);
-
-            return response()->json([
-                'deletedAt' => $order->deleted_at,
-                'status' => $order->status,
-            ]);
-        } catch (ModelNotFoundException $me) {
-            return  response()->json(['message'=>$me->getMessage()], 404);
-        } catch (\Exception $e) {
-            return  response()->json(['message'=>$e->getMessage()], 500);
-        }
-    }
+			return response()->json([
+				'deletedAt' => $order->deleted_at,
+				'status' => $order->status,
+			]);
+		} catch (ModelNotFoundException $me) {
+			return  response()->json(['message' => $me->getMessage()], 404);
+		} catch (\Exception $e) {
+			return  response()->json(['message' => $e->getMessage()], 500);
+		}
+	}
 }
