61083b96cc13affdf1b71bd17b82fb3f954827d7
Dragos <dragos360@yahoo.com>
2021-08-24 23:28:59 +0300

    orders policies

--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -4,91 +4,105 @@ namespace App\Policies;
 
 use App\Models\Order;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class OrderPolicy
 {
-    use HandlesAuthorization;
+	use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can view any models.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @return mixed
+	 */
+	public function viewAny(User $user)
+	{
+		//
+	}
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Order  $order
-     * @return mixed
-     */
-    public function view(User $user, Order $order)
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can view the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\Order  $order
+	 * @return mixed
+	 */
+	public function view(User $user, Order $order)
+	{
+		if ($user->isClient() && $order->client_id === $user->id) {
+			Response::allow();
+		} else if (!$user->isClient()) {
+			Response::allow();
+		}
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
+		Response::deny('You are not authorized to perform this action.');
+	}
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Order  $order
-     * @return mixed
-     */
-    public function update(User $user, Order $order)
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can create models.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @return mixed
+	 */
+	public function create(User $user)
+	{
+		//
+	}
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Order  $order
-     * @return mixed
-     */
-    public function delete(User $user, Order $order)
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can update the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\Order  $order
+	 * @return mixed
+	 */
+	public function update(User $user, Order $order)
+	{
+		if (($user->isWaiter() && $order->staff_id === $user->id) || $user->isAdminitrator() || $user->isLocationManager()) {
+			Response::allow();
+		}
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Order  $order
-     * @return mixed
-     */
-    public function restore(User $user, Order $order)
-    {
-        //
-    }
+		Response::deny('You are not authorized to perform this action.');
+	}
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Order  $order
-     * @return mixed
-     */
-    public function forceDelete(User $user, Order $order)
-    {
-        //
-    }
+	public function updateStatus(User $user, Order $order)
+	{
+		if (($user->isWaiter() && $order->staff_id === $user->id)
+			|| ($user->isDelivery())
+			|| $user->isKitchenManager()
+			|| $user->isAdminitrator()
+			|| $user->isLocationManager()
+		) {
+			Response::allow();
+		}
+
+		Response::deny('You are not authorized to perform this action.');
+	}
+
+	public function	updateItems(User $user, Order $order)
+	{
+		if (($user->isWaiter() && $order->staff_id === $user->id) || $user->isAdminitrator() || $user->isLocationManager()) {
+			Response::allow();
+		}
+
+		Response::deny('You are not authorized to perform this action.');
+	}
+
+	/**
+	 * Determine whether the user can delete the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\Order  $order
+	 * @return mixed
+	 */
+	public function disable(User $user, Order $order)
+	{
+		if (($user->isWaiter() && $order->staff_id === $user->id) || $user->isAdminitrator() || $user->isLocationManager()) {
+			Response::allow();
+		}
+
+		Response::deny('You are not authorized to perform this action.');
+	}
 }
